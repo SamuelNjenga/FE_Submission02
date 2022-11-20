d66ab5302645978668280778e3351e3baa869a11
Samuel Njenga <snjenga704@gmail.com>
2022-11-20 13:53:18 +0300

    feat: add index.html, index.js and style.css which handle the UI of the login component

---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+const LOGIN_BASE_URL = "https://freddy.codesubmit.io/login";
+
---
+++ src/index.js
@@ -0,0 +1,28 @@
+let loginForm = document.getElementById("loginForm");
+
+const headers_ = {
+  "Content-Type": "application/json",
+};
+
+// When the form is submitted...
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log("EH");
+  // POST the data
+  axios
+    .post(
+      LOGIN_BASE_URL,
+      {
+        username: document.getElementById("username").value,
+        password: document.getElementById("password").value,
+      },
+      { headers: headers_ }
+    )
+    .then((resp) => {
+      const tokenValue = resp.data.access_token;
+      localStorage.setItem("accessToken", tokenValue);
+    })
+    .catch(function (error) {
+      alert("The username or password is incorrect");
+    });
+});
